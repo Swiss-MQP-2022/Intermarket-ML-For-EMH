cd6de07f40f60fec55bf897c3a4e52e42a3a2f71
GrantPerkins <gcperk20@gmail.com>
2022-09-12 16:52:21 +0200

    Right roll

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ import numpy as np
 
 
 def percent_diff(array):
-    rolled = np.roll(array, -1, axis=0)
-    return (rolled - array) / array
+    rolled = np.roll(array, 1, axis=0)
+    return (array - rolled) / array
