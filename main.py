ad5ecf45c9087a84f8640ef77598d1642e3c1eec
N'yoma Diamond <nyoma@windowoffire.com>
2022-09-14 17:22:13 +0200

    classification test

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import torch
+from torch.nn import BCELoss
+from torch.nn import functional as F
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler as Scaler
+from sklearn.metrics import classification_report
+import numpy as np
 
 import utils
 from timeseries_dataset import TimeSeriesDataLoader
-from models import SimpleLSTM, LimLundgrenLoss
+from models import SimpleLSTMClassifier
+# from losses import LimLundgrenLoss
 from trainer import Trainer
 
 # Get CUDA availability
@@ -19,36 +24,31 @@ df = pd.read_csv(r'./data/stock/SPY.US.csv').set_index('timestamp')  # Load data
 df = utils.get_nonempty_float_columns(df).dropna()  # filter to numeric columns. Drop NaNs
 
 X_0 = df.iloc[0]  # record initial raw X values
-y_0 = X_0['close']  # record initial raw y value
 
-# pct_df = df.pct_change()[1:]  # Compute percent change
-# pct_df = utils.remove_outliers(pct_df)
-
-X_scaler, y_scaler = Scaler(), Scaler()  # Initialize scalers for normalization
+X_scaler = Scaler()  # Initialize scalers for normalization
 X = X_scaler.fit_transform(df[:-1])  # normalize X data
-y = y_scaler.fit_transform(df['close'].pct_change().to_numpy().reshape(-1, 1)[1:]).flatten() # normalize y data
+
+y = np.sign(df['close'].diff()).to_numpy()[1:] + 1  # convert y to direction classes
+# y = F.one_hot(y)  # one hot encode y
 
 # Put data on tensors
 X = torch.tensor(X).float()
-y = torch.tensor(y).float()
-
-# X = torch.tensor(pct_df[:-1].to_numpy()).float()
-# y = torch.tensor(pct_df['close'][1:].to_numpy()).float()
+y = F.one_hot(torch.tensor(y).long()).float()
 
 test_split = 0.20
 
 # Create data loader
-dataloader = TimeSeriesDataLoader(X, y, validation_split=0.20, test_split=test_split, period=1000, batch_size=10)
+dataloader = TimeSeriesDataLoader(X, y, validation_split=0.20, test_split=test_split, period=100, batch_size=1000)
 
 # Initialize model
-model = SimpleLSTM(X.shape[1], 100, 3, batch_first=True, dropout=0.5)
+model = SimpleLSTMClassifier(X.shape[1], 100, 3, batch_first=True, dropout=0.5)
 if cuda_available:
     model.cuda()  # put model on CUDA if present
 
 #  Initialize loss, optimizer, and scheduler
-criterion = LimLundgrenLoss()  # Loss criterion
+criterion = BCELoss()  # Loss criterion
 optim = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-2)  # Optimizer
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, factor=0.1, patience=100, verbose=True)  # Learning rate scheduler
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, factor=0.1, patience=10, verbose=True)  # Learning rate scheduler
 
 # Initialize trainer
 trainer = Trainer(model, criterion, optim, dataloader)
@@ -58,36 +58,21 @@ train_loss, validation_loss = trainer.train_loop(epochs=10, print_freq=1)
 
 print('Creating plots...')
 
-if cuda_available:
-    X = X.cuda()
-
-forecast, _ = model.forecast(X.unsqueeze(0))  # Forecast on entire X data
-forecast = forecast.flatten().cpu().detach().numpy()  # Reorganize for plotting
-
-fig, axs = plt.subplots(ncols=2, figsize=(10, 5))
-
 # loss during training
-axs[0].plot(train_loss, label='Training')
-axs[0].plot(validation_loss, label='Validation')
-axs[0].legend()
-axs[0].set_title(f'Model Loss ({criterion.__class__.__name__})')
-axs[0].set_xlabel('Epoch')
-axs[0].set_ylabel('Loss')
-
-# S&P 500 Forecasting
-axs[1].plot(utils.pct_to_cumulative(y_scaler.inverse_transform(forecast.reshape(-1, 1)).flatten(), y_0), label='Forecast')
-axs[1].plot(utils.pct_to_cumulative(y_scaler.inverse_transform(y.reshape(-1, 1)).flatten(), y_0), label='S&P 500')
-# axs[1].plot(utils.pct_to_cumulative(forecast, y_0), label='Forecast')
-# axs[1].plot(utils.pct_to_cumulative(y.numpy(), y_0), label='S&P 500')
-axs[1].legend()
-axs[1].set_title('Model Prediction vs. S&P 500')
-axs[1].set_xlabel('Time')
-axs[1].set_ylabel('Value')
-axs[1].axvline(len(y)*(1-test_split), color="tab:purple")
+plt.plot(train_loss, label='Training')
+plt.plot(validation_loss, label='Validation')
+plt.legend()
+plt.title(f'Model Loss ({criterion.__class__.__name__})')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 
 plt.savefig('./images/plot.png')
 
-# Classification report for direction accuracy
-print(utils.forecast_classification_report(forecast, y))
+if cuda_available:
+    X = X.cuda()
+
+forecast, _ = model.forecast(X)  # Forecast on entire X data
+
+print(classification_report(y.argmax(dim=1), forecast.argmax(dim=1)))
 
 print('Done!')
--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-import torch
 from torch import nn
 
 
-class SimpleLSTM(nn.Module):
+class SimpleLSTMRegressor(nn.Module):
     # Model initialization
     def __init__(self, input_size, hidden_size, num_layers, **kwargs):
         """
@@ -11,7 +10,7 @@ class SimpleLSTM(nn.Module):
         :param num_layers: layers to use within LSTM
         :param **kwargs: keyword arguments to pass to nn.LSTM
         """
-        super(SimpleLSTM, self).__init__()
+        super(SimpleLSTMRegressor, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -29,8 +28,8 @@ class SimpleLSTM(nn.Module):
         lstm_out, (_, c_n) = self.lstm(x, *args)
         memory = (lstm_out, c_n[-1])
 
-        batches = lstm_out.size(0)
-        period = lstm_out.size(1)
+        # batches = lstm_out.size(0)
+        # period = lstm_out.size(1)
 
         lin_out = self.out_layer(lstm_out[:, -1, :])
         # lin_out = self.out_layer(lstm_out.flatten(start_dim=0, end_dim=1)).view((batches, period))
@@ -46,39 +45,58 @@ class SimpleLSTM(nn.Module):
         lstm_out, (_, c_n) = self.lstm(x, *args)
         memory = (lstm_out, c_n[-1])
 
-        output = self.out_layer(lstm_out)
+        lin_out = self.out_layer(lstm_out)
+        output = self.sigmoid(lin_out)
 
         return output, memory
 
 
-class ComboLoss(nn.Module):
-    def __init__(self,
-                 c_bias=1,
-                 regression_fn=nn.functional.mse_loss,
-                 classification_fn=nn.functional.binary_cross_entropy):
+
+class SimpleLSTMClassifier(nn.Module):
+    # Model initialization
+    def __init__(self, input_size, hidden_size, num_layers, **kwargs):
         """
-        :param c_bias: weight factor used to bias classification loss
-        :param regression_fn: function for calculating regression loss
-        :param classification_fn: function for calculating classification loss
+        :param input_size: input size of the model
+        :param hidden_size: number of nodes to use per layer of LSTM
+        :param num_layers: layers to use within LSTM
+        :param **kwargs: keyword arguments to pass to nn.LSTM
         """
+        super(SimpleLSTMClassifier, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
-        super(ComboLoss, self).__init__()
-        self.regression_fn = regression_fn
-        self.classification_fn = classification_fn
-        self.c_bias = c_bias
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, **kwargs)
+        self.out_layer = nn.Linear(hidden_size, 3)  # Output layer
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, reg_pred, reg_target, cla_pred, cla_target):
-        reg_loss = self.regression_fn(reg_pred, reg_target)
-        classification_loss = self.c_bias * self.classification_fn(cla_pred, cla_target)
-        return reg_loss + classification_loss
+    # Model operation
+    def forward(self, x, *args):
+        """
+        :param x: tensor with input data for the model to operate over
+        :param *args: arbitrary arguments to provide with x. Expected to include (h_0, c_0) if available
+        """
+        lstm_out, (_, c_n) = self.lstm(x, *args)
+        memory = (lstm_out, c_n[-1])
 
+        # batches = lstm_out.size(0)
+        # period = lstm_out.size(1)
 
-class LimLundgrenLoss(nn.Module):
-    def __init__(self, epsilon=0.01):
-        super(LimLundgrenLoss, self).__init__()
-        self.epsilon = epsilon
+        lin_out = self.out_layer(lstm_out[:, -1, :])
+        # lin_out = self.out_layer(lstm_out.flatten(start_dim=0, end_dim=1)).view((batches, period))
+        output = self.softmax(lin_out)
 
-    def forward(self, pred, truth):
-        numer = (truth - pred).square().sum()
-        denom = pred.sign().eq(truth.sign()).sum() + self.epsilon
-        return numer / denom
+        return output, memory
+
+    def forecast(self, x, *args):
+        """
+        :param x: tensor with input data for the model to operate over
+        :param *args: arbitrary arguments to provide with x. Expected to include (h_0, c_0) if available
+        """
+        lstm_out, (_, c_n) = self.lstm(x, *args)
+        memory = (lstm_out, c_n[-1])
+
+        lin_out = self.out_layer(lstm_out)
+        output = self.softmax(lin_out)
+
+        return output, memory
