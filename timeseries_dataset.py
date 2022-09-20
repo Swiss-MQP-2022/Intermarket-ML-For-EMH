c35d10fbf867f87c76045c43a24b18657167476a
GrantPerkins <gcperk20@gmail.com>
2022-09-20 11:26:38 +0200

    Initial SVC

--- timeseries_dataset.py
+++ timeseries_dataset.py
@@ -1,44 +1,63 @@
 import numpy as np
-from torch.utils.data import Dataset, Subset, DataLoader
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler as Scaler
 from math import floor
 
-
-class TimeSeriesDataset(Dataset):
-    def __init__(self, X: np.ndarray, y: np.ndarray, period=10):
-        """
-        :param X: m by n input matrix. m=number of time series, n=total data points
-        :param y: n label vector
-        :param period: length of time series to train on
-        """
-        self.X = X
-        self.y = y
-        self.period = period
-
-    def __len__(self):
-        m = self.X.size(0)
-        return m - self.period
-
-    def __getitem__(self, index):
-        x = self.X[index:index+self.period]
-        y = self.y[index+self.period]
-        return x, y
+import utils
 
 
 class TimeSeriesDataLoader:
-    def __init__(self, X, y, validation_split=0.20, test_split=0.20, batch_size=4, period=100):
-        self.dataset = TimeSeriesDataset(X, y, period=period)
+    def __init__(self, X, y, period=100, validation_size=0.2, test_size=0.2, random_state=1130):
+        items, features = X.shape
+        trim_x = items % period
+        X = X[:-trim_x]
 
-        test_start = floor(len(self.dataset) * (1 - test_split))  # Starting index of testing set
-        subset_split = validation_split*(1-test_split)  # math to get correct validation split after pulling test_split
+        self.X = X.reshape(-1, period, features)
+        self.y = y[:-trim_x].reshape(-1, 1)
+        datset_length, period_length, features = self.X.shape
 
-        train_indices, validation_indices = train_test_split(range(test_start), test_size=subset_split)
-        test_indices = range(test_start, len(self.dataset))
-        self.train_dataset = Subset(self.dataset, train_indices)
-        self.validation_dataset = Subset(self.dataset, validation_indices)
-        self.test_dataset = Subset(self.dataset, test_indices)
+        test_start = floor(datset_length * (1 - test_size))  # Starting index of testing set
+        # subset_split = validation_size * (1 - test_size)
+        # train_indices, validation_indices = train_test_split(range(test_start), test_size=subset_split)
+        train_indices = range(test_start)
+        test_indices = range(test_start, len(self.X))
 
-        self.train_data_loader = DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True)
-        self.validation_data_loader = DataLoader(self.validation_dataset, batch_size=batch_size, shuffle=True)
-        self.test_data_loader = DataLoader(self.test_dataset, batch_size=batch_size, shuffle=False)
-        self.all_data_loader = DataLoader(self.dataset, batch_size=batch_size, shuffle=False)
+        self.X_train = self.X[train_indices]
+        self.y_train = self.y[train_indices]
+        # self.X_val = self.X[validation_indices]
+        # self.y_val = self.y[validation_indices]
+        self.X_test = self.X[test_indices]
+        self.y_test = self.y[test_indices]
+
+        scaler = Scaler(feature_range=(-1, 1))
+
+        def scale_x(x_input):
+            x_shape = x_input.shape
+            x_reshaped = x_input.reshape(-1, 1)
+            x_scaled = scaler.fit_transform(x_reshaped)
+            x_out = x_scaled.reshape(x_shape)
+            return x_out
+
+        for feature in range(features):
+            x = self.X_train[:, :, feature]
+            self.X_train[:, :, feature] = scale_x(x)
+
+            # x = self.X_val[:, :, feature]
+            # self.X_val[:, :, feature] = scale_x(x)
+
+            x = self.X_test[:, :, feature]
+            self.X_test[:, :, feature] = scale_x(x)
+
+
+if __name__ == "__main__":
+    spy = pd.read_csv(r'./data/stock/SPY.US.csv').set_index('date')  # Load data from file
+    spy = utils.get_nonempty_float_columns(spy).dropna()  # filter to numeric columns. Drop NaNs
+
+    pct_df = spy.pct_change()[1:]  # Compute percent change
+    pct_df = utils.remove_outliers(pct_df)
+
+    X = pct_df[:-1].to_numpy()
+    y = np.sign(pct_df['close'].to_numpy())[1:] + 1
+
+    dataloader = TimeSeriesDataLoader(X, y)
